f8f43e09b0bec9ed869a8ef76c1a3480f27cd44d
Vladislav Pipkin <pipkin.vladislav@yandex.ru>
2023-04-26 20:32:09 +0300

    Move data from template to view

--- places/views.py
+++ places/views.py
@@ -1,3 +1,40 @@
 from django.shortcuts import render
+from .models import Place
 
-# Create your views here.
+
+def index(request):
+    template = 'index.html'
+    json = {
+      "type": "FeatureCollection",
+      "features": [
+        {
+          "type": "Feature",
+          "geometry": {
+            "type": "Point",
+            "coordinates": [37.62, 55.793676]
+          },
+          "properties": {
+            "title": "«Легенды Москвы",
+            "placeId": "moscow_legends",
+            "detailsUrl": "https://raw.githubusercontent.com/devmanorg/where-to-go-frontend/master/places/moscow_legends.json"
+          }
+        },
+        {
+          "type": "Feature",
+          "geometry": {
+            "type": "Point",
+            "coordinates": [37.64, 55.753676]
+          },
+          "properties": {
+            "title": "Крыши24.рф",
+            "placeId": "roofs24",
+            "detailsUrl": "https://raw.githubusercontent.com/devmanorg/where-to-go-frontend/master/places/roofs24.json"
+          }
+        }
+      ]
+    }
+    
+    context = {
+        'json': json,
+    }
+    return render(request, template, context=context)
--- where_to_go/urls.py
+++ where_to_go/urls.py
@@ -1,13 +1,11 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
-
-from . import views
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.index),
+    path('', include('places.urls', namespace='places')),
 ]
 
 if settings.DEBUG:
