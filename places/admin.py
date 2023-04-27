d6586643a40e32a6a1720f5dc59f67f584d123d8
Vladislav Pipkin <pipkin.vladislav@yandex.ru>
2023-04-27 15:29:41 +0300

    Add sortable image in admin panel

--- places/admin.py
+++ places/admin.py
@@ -1,12 +1,14 @@
 from django.contrib import admin
 from django.utils.html import format_html
+from adminsortable2.admin import SortableTabularInline, SortableAdminBase
 
 from .models import Place, PlaceImage
 
 
-class PlaceImageInline(admin.TabularInline):
+class PlaceImageInline(SortableTabularInline):
     model = PlaceImage
     readonly_fields = ('get_preview',)
+    ordering = ('position',)
 
     def get_preview(self, obj):
         return format_html('<img src="{url}" width="{width}">',
@@ -15,7 +17,7 @@ class PlaceImageInline(admin.TabularInline):
 
 
 @admin.register(Place)
-class PlaceAdmin(admin.ModelAdmin):
+class PlaceAdmin(SortableAdminBase, admin.ModelAdmin):
     inlines = [
         PlaceImageInline,
     ]
